ff09f48a46ce38872dd4889958d568bbf8faa94f
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-14 17:27:49 -0300

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log("oi")
+import { RabbitMQReceiver, TConsumer } from "./app/receiver/receiver.js";
+
+const consumer: TConsumer = (channel) => {
+  return (message) => {
+    if (message) {
+      const pids = JSON.parse(message.content.toString())
+      console.log(pids)
+      channel.ack(message)
+    }
+  }
+}
+
+try {
+  await RabbitMQReceiver.receiver(consumer)
+} catch (error) {
+
+}
