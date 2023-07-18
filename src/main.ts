af1aa47b74109b61f0d351f24ff9dee9be132ecb
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-18 14:53:39 -0300

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
-import { RabbitMQReceiver, TConsumer } from "./app/receiver/receiver.js";
+import { Channel, ConsumeMessage } from "amqplib";
+import { RabbitMQPublisher } from "./app/publisher/publisher.js";
+import { RabbitMQReceiver } from "./app/receiver/receiver.js"
 
-const consumer: TConsumer = (channel) => {
-  return (message) => {
+
+const list = ['B09BBDVPB1']
+
+function consumer(channel: Channel) {
+  return function (message: ConsumeMessage | null){ 
     if (message) {
-      const pids = JSON.parse(message.content.toString())
-      console.log(pids)
+      setTimeout(() => {
+        console.log('gaymen')
+      }, 10000)
+      console.log(message.content.toString())
       channel.ack(message)
     }
   }
 }
 
 try {
-  await RabbitMQReceiver.receiver(consumer)
+  await RabbitMQPublisher.publish(list)
+  // console.log('Listening for messages...')
+  // await RabbitMQReceiver.receiver(consumer)
 } catch (error) {
-
+  console.error(error)
 }
