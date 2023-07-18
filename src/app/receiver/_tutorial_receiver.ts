5a885c820d47905dacb61302adaceef06873f5e7
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-18 14:53:17 -0300

    feat: update receiver to retrieve just 1 message at time

--- src/app/receiver/_tutorial_receiver.ts
+++ src/app/receiver/_tutorial_receiver.ts
@@ -1,25 +1,20 @@
-import client, { Connection, Channel, ConsumeMessage } from 'amqplib'
-import { 
-  RABBITMQ_URL,
-  RABBITMQ_MAIN_QUEUE
-} from '../settings.js'
-
+import client, { Channel, Connection, ConsumeMessage } from "amqplib";
+import { RABBITMQ_MAIN_QUEUE, RABBITMQ_URL } from "../settings.js";
 
 function consumer(channel: Channel) {
-  return function (message: ConsumeMessage | null){ 
+  return function (message: ConsumeMessage | null) {
     if (message) {
-      console.log(JSON.parse(message.content.toString()))
-      channel.ack(message)
+      console.log(JSON.parse(message.content.toString()));
+      channel.ack(message);
     }
-  }
+  };
 }
 
 try {
-  const connection: Connection = await client.connect(RABBITMQ_URL)
-  const channel: Channel = await connection.createChannel()
-  await channel.assertQueue(RABBITMQ_MAIN_QUEUE, {durable: false})
-  await channel.consume(RABBITMQ_MAIN_QUEUE, consumer(channel))
-}
-catch (error) {
-  console.error(error)
+  const connection: Connection = await client.connect(RABBITMQ_URL);
+  const channel: Channel = await connection.createChannel();
+  await channel.assertQueue(RABBITMQ_MAIN_QUEUE, { durable: false });
+  await channel.consume(RABBITMQ_MAIN_QUEUE, consumer(channel));
+} catch (error) {
+  console.error(error);
 }
--- src/app/receiver/receiver.ts
+++ src/app/receiver/receiver.ts
@@ -1,17 +1,20 @@
-import client, { Connection, Channel, ConsumeMessage } from 'amqplib'
-import { 
-  RABBITMQ_URL,
+import client, { Channel, Connection, ConsumeMessage } from "amqplib";
+import {
   RABBITMQ_MAIN_QUEUE,
-  OPTIONS_ASSERT_QUEUE
-} from '../settings.js'
+  RABBITMQ_URL,
+} from "../settings.js";
 
-export type TConsumer = (channel: Channel) => (message: ConsumeMessage | null) => void
+export type TConsumer = (
+  channel: Channel,
+) => (message: ConsumeMessage | null) => void;
 
 export class RabbitMQReceiver {
-  static async receiver(consumer: TConsumer) {
-    const connection: Connection = await client.connect(RABBITMQ_URL)
-    const channel: Channel = await connection.createChannel()
-    await channel.assertQueue(RABBITMQ_MAIN_QUEUE, OPTIONS_ASSERT_QUEUE)
-    await channel.consume(RABBITMQ_MAIN_QUEUE, consumer(channel))
+  static async receiver(message_handler: TConsumer) {
+    const connection: Connection = await client.connect(RABBITMQ_URL);
+    const channel: Channel = await connection.createChannel();
+    await channel.assertQueue(RABBITMQ_MAIN_QUEUE, { durable: false });
+    await channel.consume(RABBITMQ_MAIN_QUEUE, message_handler(channel), {
+      noAck: false,
+    });
   }
 }
