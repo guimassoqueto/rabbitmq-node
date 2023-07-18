2a2e31ef816d32db192308c73b4027a74ab9070d
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-18 14:52:43 -0300

    feat: update publisher to send just 1 message per work at time

--- src/app/publisher/_tutorial_publisher.ts
+++ src/app/publisher/_tutorial_publisher.ts
@@ -8,8 +8,8 @@ async function publisher(message: any) {
   try {
     const connection: Connection = await client.connect(RABBITMQ_URL)
     const channel: Channel = await connection.createChannel()
-    await channel.assertQueue(RABBITMQ_MAIN_QUEUE, { autoDelete: true })
-    channel.sendToQueue(RABBITMQ_MAIN_QUEUE, Buffer.from(message))
+    await channel.assertQueue(RABBITMQ_MAIN_QUEUE, { autoDelete: false })
+    channel.sendToQueue(RABBITMQ_MAIN_QUEUE, Buffer.from(message), { persistent: true })
   } catch (error) {
     console.error(error)
   }
--- src/app/publisher/publisher.ts
+++ src/app/publisher/publisher.ts
@@ -1,21 +1,24 @@
-import client, { Connection, Channel } from 'amqplib'
-import { 
-  RABBITMQ_URL,
-  RABBITMQ_MAIN_QUEUE,
-  OPTIONS_ASSERT_QUEUE
-} from '../settings.js'
+import client, { Channel, Connection } from "amqplib";
+import { RABBITMQ_MAIN_QUEUE, RABBITMQ_URL } from "../settings.js";
 
 export class RabbitMQPublisher {
   static async publish(message: any) {
-    const connection: Connection = await client.connect(RABBITMQ_URL)
-    const channel: Channel = await connection.createChannel()
-    await channel.assertQueue(RABBITMQ_MAIN_QUEUE, OPTIONS_ASSERT_QUEUE)
-    if (['number', 'string'].includes(typeof message)) {
-      channel.sendToQueue(RABBITMQ_MAIN_QUEUE, Buffer.from(message))
+    const connection: Connection = await client.connect(RABBITMQ_URL);
+    const channel: Channel = await connection.createChannel();
+    await channel.assertQueue(RABBITMQ_MAIN_QUEUE, { durable: false });
+    await channel.prefetch(1); // will send one message at time
+    if (["number", "string"].includes(typeof message)) {
+      channel.sendToQueue(RABBITMQ_MAIN_QUEUE, Buffer.from(message), {
+        persistent: true,
+      });
     }
-    if (typeof message == 'object') {
-      channel.sendToQueue(RABBITMQ_MAIN_QUEUE, Buffer.from(JSON.stringify(message)))
-    } 
+    if (typeof message == "object") {
+      channel.sendToQueue(
+        RABBITMQ_MAIN_QUEUE,
+        Buffer.from(JSON.stringify(message)),
+        { persistent: true },
+      );
+    }
     setTimeout(() => {
       channel.close();
       connection.close();
